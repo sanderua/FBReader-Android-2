005f9121f0b3a9526c08b0c8bde55bd03ed12b0f
aplicatii romanesti <aplicatii.romanesti@gmail.com>
2018-06-23 23:21:31 +0300

    Android api 28; Fix cpp and mks to accept new NDK

--- fbreader/text-formats/src/main/jni/Application.mk
+++ fbreader/text-formats/src/main/jni/Application.mk
@@ -1,2 +1,3 @@
-APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
-APP_STL := stlport_static
+APP_ABI :=  arm64-v8a armeabi-v7a x86 x86_64
+#APP_STL := stlport_static # deprecated, use either c++_static or c++_shared. See https://developer.android.com/ndk/guides/cpp-support.html
+APP_STL := c++_static # newer
--- fbreader/zip-amse/src/main/jni/Application.mk
+++ fbreader/zip-amse/src/main/jni/Application.mk
@@ -1,2 +1,3 @@
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
-APP_STL := stlport_static
+#APP_STL := stlport_static # deprecated, use either c++_static or c++_shared. See https://developer.android.com/ndk/guides/cpp-support.html
+APP_STL := c++_static
